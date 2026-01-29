29f602a8f86cd0f8b156bc2ed58e318999a3f38e
Ashrithhn <ashrith.sringeri@github.com>
2026-01-29 11:11:19 +0530

    add basic invitation editor layout

---
+++ src/components/invitation/InvitationEditor.jsx
@@ -0,0 +1,10 @@
+function InvitationEditor() {
+  return (
+    <section className="invitation-editor">
+      <h2>Edit Invitation</h2>
+      <input placeholder="Event Title" />
+    </section>
+  );
+}
+
+export default InvitationEditor;
